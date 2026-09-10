de920b1ebd04d0f357462740819a6252110d7e7c
alok-666 <alokmalik8261@gmail.com>
2026-09-10 19:50:46 +0530

    Add files via upload

---
+++ Lecture-7/practice questions/practice question 1.py
@@ -0,0 +1,4 @@
+#creating a new file "practice.txt" using python and enter given data.
+with open("practicee.txt","w") as file:
+     file.write("Hi everyone.\nWe are learning file I/O.")
+     file.write("Using java.\nI like programming in java.")
