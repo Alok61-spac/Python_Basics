5dd5d7eb72008c14fa0001e8db9af70c20580af7
alok-666 <alokmalik8261@gmail.com>
2026-09-10 19:42:43 +0530

    Delete Lecture-6 directory

--- Lecture-6/Function/length.py
+++
@@ -1,8 +0,0 @@
-#print the lenth of list
-def print_len(list):
-    print(len(list))
-
-subject =["math","science","history","geography","sanskrit"]
-marks = [34,63,93,73,92,874,27,983,28,5]
-print_len(marks)
-print_len(subject)
