13b4230dee6e34c8fd05dbf3bf473c50c7466650
alok-666 <alokmalik8261@gmail.com>
2026-09-10 19:47:45 +0530

    Add files via upload

---
+++ Lecture-4/Dictionary/basics_of_my_dictionary.py
@@ -0,0 +1,13 @@
+#printing of information in dictionary
+my_Dict = {
+    "key pair":"value pair",
+    "name": ("Mumbai","Delhi","Hyderabad"),
+    "class":[12,7,8,9],
+    "age":[89.5,45.6,61.9]
+
+}
+print(my_Dict)
+#print length of dictionary
+print(len(my_Dict))
+#print type of dictionary
+print(type(my_Dict))
